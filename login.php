f74da0f095cd10433b3684f118c1c05fade776f7
Ethan Andre <andreverallo14@gmail.com>
2025-06-19 14:07:02 +0800

    ANOTHER UPDATE

--- login.php
+++ login.php
@@ -1,10 +1,14 @@
+<?php
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
-    <title>BradPoints</title>
+    <title>BradPoints Login</title>
 </head>
 <body class="loginbody">
     <?php include "includes/login.html" ?>
