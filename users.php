13c763d218f5513d796a67d509d3ba574283fbd3
Ethan Andre <andreverallo14@gmail.com>
2025-07-13 18:17:32 +0800

    Users Admin

--- users.php
+++ users.php
@@ -16,12 +16,59 @@ session_start();
         $user_data = mysqli_fetch_assoc($result);
     }
 
+    //Create user
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['add_user'])) {
+        $username = trim($_POST['username']);
+        $points = intval($_POST['points']);
+        if ($username !== '') {
+            $create_query = "INSERT INTO users (user_name, points, usertype_id) VALUES (?, ?, 2)";
+            $stmt = mysqli_prepare($con, $create_query);
+            mysqli_stmt_bind_param($stmt, "si", $username, $points);
+            if (mysqli_stmt_execute($stmt)) {
+                $success_message = "User added!";
+            } else {
+                $error_message = "Failed to add user.";
+            }
+            mysqli_stmt_close($stmt);
+        }
+    }
+
+    //Update user
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['edit_user'])) {
+        $edit_id = intval($_POST['edit_id']);
+        $edit_username = trim($_POST['edit_username']);
+        $edit_points = intval($_POST['edit_points']);
+        $edit_query = "UPDATE users SET user_name=?, points=? WHERE id=?";
+        $stmt = mysqli_prepare($con, $edit_query);
+        mysqli_stmt_bind_param($stmt, "sii", $edit_username, $edit_points, $edit_id);
+        if (mysqli_stmt_execute($stmt)) {
+            $success_message = "User updated!";
+        } else {
+            $error_message = "Failed to update user.";
+        }
+        mysqli_stmt_close($stmt);
+    }
+
+    //Delete user
+    if (isset($_GET['delete'])) {
+        $delete_id = intval($_GET['delete']);
+        $delete_query = "DELETE FROM users WHERE id=?";
+        $stmt = mysqli_prepare($con, $delete_query);
+        mysqli_stmt_bind_param($stmt, "i", $delete_id);
+        if (mysqli_stmt_execute($stmt)) {
+            $success_message = "User deleted!";
+        } else {
+            $error_message = "Failed to delete user.";
+        }
+        mysqli_stmt_close($stmt);
+    }
+
     $all_users = [];
 
     $users_query = "SELECT id, user_name, points FROM users WHERE usertype_id = 2";
     $users_results = mysqli_query($con, $users_query);
 
-    if ($users_query) {
+    if ($users_results) {
         while ($row = mysqli_fetch_assoc($users_results)) {
             $all_users[] = $row;
         }
