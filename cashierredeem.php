48bad43cd78478429c288c000e8af0d881e755a0
Newin <154362153+solusguy@users.noreply.github.com>
2025-07-14 22:28:10 +0800

    Cashier updated css

--- cashierredeem.php
+++ cashierredeem.php
@@ -51,6 +51,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cashier Redeem</title>
     <link rel="stylesheet" href="cashier.css">
+    <link href="https://fonts.cdnfonts.com/css/coolvetica-2" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
 </head>
 <body>
     <?php include("includes/cashierredeem.html"); ?>
